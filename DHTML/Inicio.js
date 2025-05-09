4efa76da7f7e3c652cacf7e10207e12ef06a8e45
ThiagoRuiz <thiagojairruiz@gmail.com>
2025-05-09 09:24:02 -0300

    Add files via upload

---
+++ DHTML/Inicio.js
@@ -0,0 +1,9 @@
+document.getElementById("titulo").innerHTML = "Ya no es una prueba chavales"
+document.getElementById("titulo").style.color ="271aed"
+
+let num1 = parseInt(prompt("Ingrese un numero 1: "))
+let num2 = parseInt(prompt("Ingrese un numero 2"))
+
+let suma = num1 + num2
+
+document.getElementById("titulo").innerHTML = "la suma es: " + suma
